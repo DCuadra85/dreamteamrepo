9dddd08fabd4db5a15d60b715bd58f858378134d
reng <67654603+engrebecca@users.noreply.github.com>
2020-10-01 11:04:51 -0700

    Add COVID API

--- yelp.js
+++ yelp.js
@@ -1,5 +1,5 @@
 $.ajax({
-    url: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=pizza&location=san+francisco",
+    url: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=pizza&location=oakland",
     method: "GET",
     dataType: 'json',
     headers: {
@@ -8,3 +8,18 @@ $.ajax({
   }).then(function(response) {
     console.log(response);
   })
+
+  var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://covid-19-statistics.p.rapidapi.com/provinces?iso=USA",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "covid-19-statistics.p.rapidapi.com",
+		"x-rapidapi-key": "01023f3f8bmsh79a54f21d151621p10c1f7jsn246ba5f2e27a"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
