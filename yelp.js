58baf9bab290cd59f73b752dfc8e41ea606c32b2
reng <67654603+engrebecca@users.noreply.github.com>
2020-10-01 17:54:07 -0700

    Create variables for user inputs and add placeholders

--- yelp.js
+++ yelp.js
@@ -1,5 +1,22 @@
+// Update to select id val from html
+let term = "burrito";
+let city ="oakland";
+let radius = 20;
+let radiusMeters = calculateMeters(radius);
+function calculateMeters (radius){
+  if (radius === 5){
+    newRadius = 8047;
+  } else if (radius === 10){
+    newRadius = 16093
+  } else {
+    newRadius = 32187;
+  }
+  return newRadius;
+}
+let queryUrl = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${city}&radius=${radiusMeters}`;
+
 $.ajax({
-    url: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=pizza&location=oakland",
+    url: queryUrl,
     method: "GET",
     dataType: 'json',
     headers: {
@@ -7,4 +24,5 @@ $.ajax({
     }
   }).then(function(response) {
     console.log(response);
+    // append results as a list in an empty div
   })
